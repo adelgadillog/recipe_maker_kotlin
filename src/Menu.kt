4414c9ae7ba8acfbc3054e393fc26f539747ff89
Alejandro Delgadillo Garcia <4lejo.1017@gmail.com>
2020-12-14 21:30:58 -0500

    Commit: se crea el proyecto

---
+++ src/Menu.kt
@@ -0,0 +1,24 @@
+fun Menu(){
+    createMenu()
+}
+
+fun createMenu(){
+    val options : List<String>  = listOf("","Hacer una receta","Ver mis recetas","Salir")
+    println("Selecciona la opcion deseada \n")
+    for ((index,option) in options.withIndex()){
+        if(index != 0)  println("$index: $option ")
+    }
+    val response:String? = readLine()
+    when(response?.toInt()){
+        1 -> makeRecipe()
+        2 -> seeRecipe()
+        3 -> createMenu()
+    }
+}
+
+fun makeRecipe(){
+
+}
+fun seeRecipe(){
+
+}
