500662dbbb3dc99f579fb0d72201219167ebfea4
Alejandro Delgadillo Garcia <4lejo.1017@gmail.com>
2020-12-15 20:41:08 -0500

    Commit: reto 2

--- src/Menu.kt
+++ src/Menu.kt
@@ -11,15 +11,31 @@ fun createMenu(){
     val response:String? = readLine()
     when(response?.toInt()){
         1 -> makeRecipe()
-        2 -> seeRecipe()
+        2 -> viewRecipe()
         3 -> createMenu()
     }
 }
 
 fun makeRecipe(){
+    println("Hacer receta\n" +
+            "Selecciona por categoría el ingrediente que buscas")
+    val options: List<String> = listOf("","Agua","Leche","Carne","Verduras","Frutas","Cereal","Huevos","Aceites")
+    for ((index,option) in options.withIndex()){
+        if(index != 0)  println("$index: $option ")
+    }
+    val response:String? = readLine()
+    val recipe = options[response?.toInt()!!]
+    viewRecipe(recipe.toString())
+
 
 }
-fun seeRecipe(){
+fun viewRecipe(recipe:String){
+
+    println("Ver mis recetas con: $recipe")
+
+}
+
+fun viewRecipe(){
 
 
 }
